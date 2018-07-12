62c77526e86ef7e1a6de403de3a246f2ded1829a
unknown <zhangshanting@vcredit.cn>
2018-07-12 11:14:09 +0800

    反向输出列表

--- LeetCode/stack.py
+++ LeetCode/stack.py
@@ -1,33 +1,24 @@
- 
 from pythonds.basic import Stack
- 
-def parChecker(symbolString):
-    s = Stack()
-    balanced = True
-    index = 0
-    while index < len(symbolString) and balanced:
-        symbol = symbolString[index]
-        if symbol == '(':
-            s.push(symbol)
-        else:
-            if s.isEmpty():
-                balanced = False
+
+class Solution:
+    def isPopOrder(self, pushV, popV):
+        stack = []
+        j = 0
+        for i in pushV:
+            stack.append(i)
+        while stack:
+            if stack[-1] == popV[j]:
+                stack.pop()
+                j += 1
             else:
-                s.pop()
-        index = index + 1
-        
-    if balanced and s.isEmpty():
-        return True
-    else:
-        return False
-        
-        
-print(parChecker('((()())'))
-print(parChecker('((())))'))
-
-
-
-
-
-
-        
+                break
+                
+        if stack:
+            return False
+        else:
+            return True
+            
+            
+s = Solution()
+print(s.isPopOrder([1,2,3,4,5], [5,4,3,2,1]))
+print(s.isPopOrder([1,2,3,4,5], [5,4,3,1,2]))
